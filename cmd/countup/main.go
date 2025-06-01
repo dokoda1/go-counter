af016dcfecf69022559cdfb4cbd608169f5e9785
dokoda1 <109193365+dokoda1@users.noreply.github.com>
2025-06-01 17:29:01 +0900

    a
    a

--- cmd/countup/main.go
+++ cmd/countup/main.go
@@ -1,8 +1,52 @@
 package main
 
-import "github.com/dokoda1/go-counter"
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/gen"
+
+	"github.com/dokoda1/go-counter"
+	"github.com/dokoda1/go-counter/interface/db"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	// ...
 	counter.Count(1, 10)
+
+	//func loadEnvを呼び出します。
+	loadEnv()
+
+	g := gen.NewGenerator(gen.Config{
+		OutPath:           "./interface/db/query",
+		Mode:              gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+		FieldWithIndexTag: true,                                                               // modelのgormタグのindex節を生成する
+		FieldWithTypeTag:  true,                                                               // modelのgormタグのtype節を生成する
+		FieldNullable:     true,                                                               // null許容カラムのときポインタ型にする
+	})
+
+	//
+	sqlHandler, err := db.NewSqlHandler(os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	g.UseDB(sqlHandler.Conn) // reuse your gorm db
+}
+
+// .envを呼び出します。
+func loadEnv() {
+	// ここで.envファイル全体を読み込みます。
+	// この読み込み処理がないと、個々の環境変数が取得出来ません。
+	// 読み込めなかったら err にエラーが入ります。
+	err := godotenv.Load("../../../go-counter.env")
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
+
+	// .envの SAMPLE_MESSAGEを取得して、messageに代入します。
+	message := os.Getenv("SAMPLE_MESSAGE")
+
+	fmt.Println(message)
 }
